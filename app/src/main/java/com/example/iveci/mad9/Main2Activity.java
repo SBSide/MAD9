d35959f160984db0d7fcac174b17b71560ac80ff
Side <ivecis@live.co.kr>
2017-05-17 14:05:22 +0900

    HW #9

--- app/src/main/java/com/example/iveci/mad9/Main2Activity.java
+++ app/src/main/java/com/example/iveci/mad9/Main2Activity.java
@@ -11,6 +11,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -36,6 +37,7 @@ import java.util.Calendar;
 public class Main2Activity extends AppCompatActivity {
     LinearLayout l1,l2;
     ListView listView;
+    String editfile;
     ArrayList<String> list = new ArrayList<>();
     ArrayAdapter adapter;
     DatePicker datePicker;
@@ -45,15 +47,24 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Memo");
         setContentView(R.layout.activity_main2);
         int permissioninfo = ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permissioninfo == PackageManager.PERMISSION_GRANTED) makedir();
+        if (permissioninfo == PackageManager.PERMISSION_GRANTED) {
+            File file = new File(getExternalPath() + "diary");
+            if (!file.exists()) {
+                file.mkdir();
+                String msg = "디렉터리 생성";
+                if (file.isDirectory() == false) msg = "디렉터리 생성 오류";
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            }
+        }
         else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE)){
                 Toast.makeText(getApplicationContext(),
-                        "SDCard 쓰기 권한이 필요합니다. \n설정에서 수동으로 활성화해주세요.",Toast.LENGTH_SHORT).show();
+                        "SDCard 쓰기 권한이 필요합니다. \n" + "설정에서 수동으로 활성화해주세요.",Toast.LENGTH_SHORT).show();
             }
             else {
                 ActivityCompat.requestPermissions(this,
@@ -73,10 +84,10 @@ public class Main2Activity extends AppCompatActivity {
         bsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (bsave.getText().toString().equals("수정")){
-
+                if (bsave.getText().toString().equals("수정")){ //수정버튼
+                    edit();
                 }
-                else{
+                else{ //저장버튼
                     try {
                         String path = getExternalPath();
                         NumberFormat numberFormat = NumberFormat.getNumberInstance();
@@ -85,11 +96,19 @@ public class Main2Activity extends AppCompatActivity {
                                 datePicker.getYear() % 100 : datePicker.getYear();
                         int month = datePicker.getMonth()+1;
                         int day = datePicker.getDayOfMonth();
-                        BufferedWriter bw = new BufferedWriter(new FileWriter(path + "diary/" +
-                                year + "-" +  numberFormat.format(month) + "-" + numberFormat.format(day) + ".txt"));
-                        bw.write(editText.getText().toString());
-                        bw.close();
-                        Toast.makeText(getApplicationContext(),"저장완료",Toast.LENGTH_SHORT).show();
+                        File file = new File(path + "diary/" +
+                                year + "-" +  numberFormat.format(month) + "-" + numberFormat.format(day) + ".txt");
+                        if (file.exists()){ //저장할 파일명이 이미 존재하는경우
+                            editfile = year + "-" +  numberFormat.format(month) + "-" + numberFormat.format(day) + ".txt";
+                            load();
+                        }
+                        else{  //없는경우 새로저장
+                            BufferedWriter bw = new BufferedWriter(new FileWriter(path + "diary/" +
+                                    year + "-" +  numberFormat.format(month) + "-" + numberFormat.format(day) + ".txt"));
+                            bw.write(editText.getText().toString());
+                            bw.close();
+                            Toast.makeText(getApplicationContext(),"저장완료",Toast.LENGTH_SHORT).show();
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -99,7 +118,8 @@ public class Main2Activity extends AppCompatActivity {
         bcancel = (Button) findViewById(R.id.btncancel);
         bcancel.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {  //취소
+                bsave.setText("저장");
                 editText.setText("");
                 l1.setVisibility(View.VISIBLE);
                 l2.setVisibility(View.INVISIBLE);
@@ -114,20 +134,25 @@ public class Main2Activity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                //항목 선택시 수정모드
+                editfile = list.get(position);
+                load();
             }
         });
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                //항목 길게 선택시 삭제
                 AlertDialog.Builder dlg = new AlertDialog.Builder(Main2Activity.this);
                 dlg.setTitle("항목삭제")
                         .setMessage("항목을 삭제하시겠습니까?")
                         .setPositiveButton("YES", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                //실제 삭제
+                                File file = new File(getExternalPath() + "diary/" + list.get(position));
+                                file.delete();//실제 삭제
                                 list.remove(position);
+                                listdir(list);
                                 adapter.notifyDataSetChanged();
                                 Toast.makeText(getApplicationContext(),
                                         "삭제되었습니다.", Toast.LENGTH_SHORT).show();
@@ -147,7 +172,6 @@ public class Main2Activity extends AppCompatActivity {
             sdPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
         }
         else sdPath = getFilesDir() + "";
-        Toast.makeText(getApplicationContext(), sdPath, Toast.LENGTH_SHORT).show();
         return sdPath;
     }
 
@@ -162,13 +186,38 @@ public class Main2Activity extends AppCompatActivity {
         }
     }
 
-    public void makedir(){
-        String path = getExternalPath();
-        File file = new File(path + "diary");
-        file.mkdir();
-        String msg = "디렉터리 생성";
-        if (file.isDirectory() == false) msg = "디렉터리 생성 오류";
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    public void load(){ //수정모드진입
+        try {
+            l1.setVisibility(View.INVISIBLE);
+            l2.setVisibility(View.VISIBLE);
+            int y = Integer.parseInt("20" + editfile.substring(0,2));
+            int m = Integer.parseInt(editfile.substring(3,5))-1;
+            int d = Integer.parseInt(editfile.substring(6,8));
+            datePicker.init(y,m,d,null);
+            BufferedReader br  = new BufferedReader(new FileReader(getExternalPath() + "diary/" + editfile));
+            String readStr = "";
+            String str = null;
+            while((str = br.readLine()) != null) readStr += str + "\n";
+            br.close();
+            bsave.setText("수정");
+            editText.setText(readStr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"File not found", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void edit(){ //수정작업을 진행(덮어쓰기)
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(getExternalPath() + "diary/" + editfile));
+            bw.write(editText.getText().toString());
+            bw.close();
+            Toast.makeText(getApplicationContext(),"수정완료",Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void listdir(ArrayList<String> str){
@@ -181,36 +230,7 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public void onClick(View v){
-        if (v.getId() == R.id.btn1){
-            l1.setVisibility(View.INVISIBLE);
-            l2.setVisibility(View.VISIBLE);
-        }
-        else if (v.getId() == R.id.boutread){
-            try {
-                String path = getExternalPath();
-                BufferedReader br  = new BufferedReader(new FileReader(path + "diary/" + "test.txt"));
-                String readStr = "";
-                String str = null;
-                while((str = br.readLine()) != null) readStr += str + "\n";
-                br.close();
-                Toast.makeText(this, readStr.substring(0, readStr.length()-1), Toast.LENGTH_SHORT).show();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(this,"File not found", Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else if (v.getId() == R.id.boutwrite){
-            try {
-                String path = getExternalPath();
-                BufferedWriter bw = new BufferedWriter(new FileWriter(path + "diary/" + "test3.txt",true));
-                bw.write("안녕하세요 SDCard Hello");
-                bw.close();
-                Toast.makeText(this,"저장완료",Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        l1.setVisibility(View.INVISIBLE);
+        l2.setVisibility(View.VISIBLE);
     }
 }
